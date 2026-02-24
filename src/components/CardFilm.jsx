a921bb0e9f3590e9f8564e785f56d9aa33629596
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-24 17:23:30 +0100

    feat: resa dinamica CardBook tramite props e configurata HomePage con dati finti

--- src/components/CardFilm.jsx
+++ src/components/CardFilm.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
 
-const CardFilm = () => {
+const CardFilm = (props) => {
+
+     const { id, title, director, abstract, image } = props.movieProp;
 
     return (
         <div className="card mb-4">
-            <img src="https://www.jdandj.com/uploads/8/0/0/8/80083458/s611371146998849390_p82_i2_w1600.jpeg"
-                className="card-img-top" alt="titolo del film" />
+            <img src={image}
+                className="card-img-top" alt={title} />
             <div className="card-body">
-                <h5 className="card-title">Titolo del film</h5>
-                <address><i>Regista</i></address>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta ratione veritatis sunt molestias nihil odit totam, quis corporis laboriosam excepturi. Labore vero molestiae eveniet explicabo esse iure laborum nemo odio.</p>
-                <Link to={`films/1`} className="btn btn-primary">See more</Link>
+                <h5 className="card-title">{title}</h5>
+                <address><i>By {director}</i></address>
+                <p className="card-text">{abstract}</p>
+                <Link to={`films/${id}`} className="btn btn-primary">See more</Link>
             </div>
         </div>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,78 @@
 // importiamo conponente CardFilm
 import CardFilm from "../components/CardFilm"
 
+// dati temporanei per test props
+const movies = [
+    {
+        "id": 1,
+        "title": "Inception",
+        "director": "Christopher Nolan",
+        "genre": "Science Fiction",
+        "release_year": 2010,
+        "abstract": "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+        "image": "http://localhost:3000/img/movies/inception.jpg",
+        "created_at": "2024-11-29T10:40:13.000Z",
+        "updated_at": "2025-05-22T10:55:27.000Z"
+    },
+    {
+        "id": 2,
+        "title": "The Godfather",
+        "director": "Francis Ford Coppola",
+        "genre": "Crime",
+        "release_year": 1972,
+        "abstract": "The story of a powerful Italian-American crime family and their struggles.",
+        "image": "http://localhost:3000/img/movies/interstellar.jpg",
+        "created_at": "2024-11-29T10:40:13.000Z",
+        "updated_at": "2025-05-22T10:55:27.000Z"
+    },
+    {
+        "id": 3,
+        "title": "Titanic",
+        "director": "James Cameron",
+        "genre": "Romance",
+        "release_year": 1997,
+        "abstract": "A romantic story set against the tragic sinking of the RMS Titanic.",
+        "image": "http://localhost:3000/img/movies/matrix.jpg",
+        "created_at": "2024-11-29T10:40:13.000Z",
+        "updated_at": "2025-05-22T10:55:27.000Z"
+    },
+    {
+        "id": 4,
+        "title": "The Matrix",
+        "director": "The Wachowskis",
+        "genre": "Action",
+        "release_year": 1999,
+        "abstract": "A hacker discovers the truth about his reality and his role in the war against its controllers.",
+        "image": "http://localhost:3000/img/movies/the_godfather.jpg",
+        "created_at": "2024-11-29T10:40:13.000Z",
+        "updated_at": "2025-05-22T10:55:27.000Z"
+    },
+    {
+        "id": 5,
+        "title": "Interstellar",
+        "director": "Christopher Nolan",
+        "genre": "Science Fiction",
+        "release_year": 2014,
+        "abstract": "A team of explorers travels through a wormhole in space to save humanity.",
+        "image": "http://localhost:3000/img/movies/titanic.jpg",
+        "created_at": "2024-11-29T10:40:13.000Z",
+        "updated_at": "2025-05-22T10:55:27.000Z"
+    }
+]
+
 const HomePage = () => {
+
+    // funzione di rendering del listato dei film
+
+    const rederMovies = () => {
+        return movies.map(movie => {
+            return (
+                <div className="col" key={movie.id}>
+                    <CardFilm movieProp={movie} />
+                </div>
+            )
+        })
+    }
 
     return (
         <>
@@ -10,14 +81,8 @@ const HomePage = () => {
 
             <div>
                 <div className="row row-cols-3 mt-4">
-                    <CardFilm />
+                    {rederMovies()}
 
-                    <CardFilm />
-                    <CardFilm />
-                    <CardFilm />
-                    <CardFilm />
-                    <CardFilm />
-                    <CardFilm />
                 </div>
             </div>
         </>
