1221e322529f1ed96527a44e427837d933f37788
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-25 17:51:30 +0100

    feat: aggiunta pulizia dei campi e reset dello stato dopo l'invio della review

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -10,12 +10,16 @@ const ReviewForm = (props) => {
     // creiamo stringa di ref all'endpoint del BE
     const endpoint = `http://localhost:3000/api/movies/${movie_id}/reviews`;
 
-    // var di stato per gestire oggetto dei valori input form
-    const [formData, setFormData] = useState({
+
+    // creo oggetto per valori di default form
+    const initialFormData = {
         name: "",
         text: "",
         vote: 0
-    });
+    };
+
+    // var di stato per gestire oggetto dei valori input form
+    const [formData, setFormData] = useState(initialFormData);
 
 
     // funzione di gestione dei dati del form
@@ -32,7 +36,9 @@ const ReviewForm = (props) => {
         e.preventDefault();
         // gestisci la chiamata in post all'endpoint in axios
         axios.post(endpoint, formData, { headers: { 'Content-Type': 'application/json' } })
-            .then(() => { console.log("tuttt'apposto!") })
+            .then(() => {
+                setFormData(initialFormData);
+            })
             .catch((err) => {
                 console.log(err);
             })
