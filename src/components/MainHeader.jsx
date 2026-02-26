0cfe7bfa5dcca75938fac9615a6b43625401ad02
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-26 12:45:27 +0100

    style: aggiunto stile alla navbar

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom"
 
 const MainHeader = () => {
-
     return (
-        <nav className="navbar bg-body-tertiary mb-4">
-            <div className="container-fluid justify-content-start">
-                <Link className="navbar-brand" to="/">MovieHub</Link>
+        <nav className="custom-navbar mb-4">
+            <div className="container-fluid">
+                <Link className="custom-brand" to="/">
+                    MovieHub
+                </Link>
             </div>
         </nav>
-
     )
-
 }
 
 export default MainHeader
